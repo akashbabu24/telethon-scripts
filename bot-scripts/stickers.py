97ee69937ef014a71af337fa19eeabf0faa4b512
Akash <akashbabu24@yahoo.co.in>
2022-03-12 11:01:46 +0000

    sticker rotation script

---
+++ bot-scripts/stickers.py
@@ -0,0 +1,44 @@
+from telethon.tl.functions.messages import GetAllStickersRequest
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
+import time
+import random
+from telethon.sync import TelegramClient
+from telethon import functions, types
+
+session_list = ['user1', 'user2']
+time_ = [100, 250, 360, 560, 650]
+#[100, 210, 340, 450, 560]
+sticker_pack = [1, 2, 3, 4, 5]
+
+while True:
+ namechosen = random.choice(session_list)
+
+ if namechosen == 'user1':
+    api = ''
+    api_hash = ''
+ elif namechosen == 'user2' :
+    api = ''   #insert api of your user1
+    api_hash = ''   #insert hash of user2 from https://my.telegram.org/apps
+ else:
+    api = ''
+    api_hash = ''
+
+ with  TelegramClient(namechosen, api, api_hash) as client:
+    # Get all the sticker sets this user has
+  sticker_sets = client(GetAllStickersRequest(0))
+
+# Choose a sticker set
+  sticker_set = sticker_sets.sets[random.choice(sticker_pack)]
+
+# Get the stickers for this sticker set
+  stickers = client(GetStickerSetRequest(
+    stickerset=InputStickerSetID(
+        id=sticker_set.id, access_hash=sticker_set.access_hash
+    )
+  ))
+ 
+  chosen_sticker = random.choice(stickers.documents)
+# Stickers are nothing more than files, so send that
+  client.send_file('channel', chosen_sticker)
+ time.sleep(random.choice(time_))
